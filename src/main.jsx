a04e024230bdaa78dd48dccf858870abdac18637
Gracjan <g.drozdek@ixtensa.de>
2023-01-15 18:02:56 +0100

    init styled components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App';
+import { theme } from './Theme';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './globalStyles';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
